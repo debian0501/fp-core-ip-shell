07ffa16b3820ba8dd2e5d9b7407e68346fffe91b
Stefan Lopez Romero <stefan.lopez@maibornwolff.de>
2019-07-18 07:26:36 +0200

    cleanup

--- tictactoe-shell/src/main/java/de/maibornwolff/tictactoeshell/controller/GameController.java
+++ tictactoe-shell/src/main/java/de/maibornwolff/tictactoeshell/controller/GameController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-import static de.maibornwolff.tictactoecore.business.HelloEta.sayHello;
 import static de.maibornwolff.tictactoecore.business.Game.newGame;
 import static de.maibornwolff.tictactoecore.business.Game.game;
 
@@ -14,12 +13,6 @@ public class GameController {
 
     private GameState gameState = newGame();
 
-
-    @RequestMapping("/greeting")
-    public Hello greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Hello(sayHello(name));
-
-    }
 
     @GetMapping("/newGame")
     public GameState startGame() {
